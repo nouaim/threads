66d483848575ba3c9305fe0ef970ed2b09ad0a4c
nuaip <mebarkinouaim@gmail.com>
2023-10-31 16:16:11 +0100

    improve leftsidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
 import { sidebarLinks } from "@/constants";
 import Link from "next/link"
 import Image from "next/image"
@@ -33,6 +34,25 @@ function LeftSidebar() {
                 }
                 )
                 }
+
+                <div className="mt-10 px-6">
+                    <SignedIn>
+                        <SignOutButton signOutCallback={() => router.push('/sign-in')}>
+                            <div className="flex cursor-pointer gap-4 p-4">
+                                <Image
+                                    src="/assets/logout.svg"
+                                    alt="logout"
+                                    width={24}
+                                    height={24} 
+                                />
+
+                            <p className="text-light-2 max-lg:hidden">Logout</p>
+                            </div>
+                        </SignOutButton>
+                    </SignedIn>
+                </div>
+
+
             </div>
         </section>
     )
